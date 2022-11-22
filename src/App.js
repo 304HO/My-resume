c9791147f6e803bbd0d0e10d7bb503a7daec174a
304HO <lonnie4165@gmail.com>
2022-11-22 20:33:56 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<Main />} />
         <Route path="/review" element={<Review />} />
